d581771d8a22b46e4c3fa2ba3452c4d7cc85298c
jsalazar2019070 <jsalazar-2019070@kinal.edu.gt>
2020-09-02 16:16:25 -0600

    Se creo la clase de Conexión

--- src/java/configuracion/Conexion.java
+++ src/java/configuracion/Conexion.java
@@ -1,26 +1,14 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package configuracion;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-
-
+/**
+ *
+ * @author LAPTOP
+ */
 public class Conexion {
-    Connection conexion;
     
-    public Conexion() {
-        try{
-            Class.forName("com.mysql.jdbc.driver");
-            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/DBRegistro?useSSL=false","root","admin");
-            
-        }catch(Exception e){
-            e.printStackTrace();
-            System.out.print("no se pudo establecer una conexion" + e);
-        }
-    
-    }
-    public Connection getConnection(){
-        return conexion;
-    }
-           
 }
