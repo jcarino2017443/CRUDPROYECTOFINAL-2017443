905facb7d086089840f416782b55a87c8d8a2708
Emerson <Rosales>
2020-09-03 12:53:03 -0600

    Se creo la clase PersonaDao

--- src/java/modeloDAO/PersonaDAO.java
+++ src/java/modeloDAO/PersonaDAO.java
@@ -1,3 +1,4 @@
+/*Emerson Alexsander Rosales Ixcoy - 2019380*/ 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
